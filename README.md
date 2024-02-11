bed5abe3990e404c16bdb511c2ff014f80e22acc
Ahmed Abdelfattah <ahmed.a.abdelfattah@mercedes-benz.com>
2024-02-11 01:11:03 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # finfun
-Fetch stock data from yfinance and publish it to 
+Fetch stock data from yfinance and publish it to a google sheet named `FinFun`
+
+# dependencies
+
+Install the following python packages
+
+```
+pip3 install pygsheets
+pip3 install yfinance --upgrade --no-cache-dir
+```
+
+To access google sheets you need OAuthCredentials, this can be obtained following this [tutorial](https://pygsheets.readthedocs.io/en/latest/authorization.html). Afterwards, download the secret file and save it to the same project path as `client_secret.json`
+
+Also you should create `FinFun` sheet into your google account.
+
+# Running
+
+Fill the stocks you are interested in into `stocks.json`
+`python3 finfun.py`
---
+++ finfun.py
@@ -0,0 +1,63 @@
+import yfinance as yf
+import pandas as pd
+import json
+import pygsheets
+
+def get_stock_data(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        hist_5y = stock.history(period="5y")
+        hist_1y = stock.history(period="1y")
+        hist_alltime = stock.history(period="max")
+
+        last_5_years_return = (hist_5y.iloc[-1]['Close'] - hist_5y.iloc[0]['Close']) / hist_5y.iloc[0]['Close']
+
+        return {
+            'symbol': ticker,
+            'price': stock.info.get('currentPrice', None),
+            '52_high': hist_1y['Close'].max(),
+            'all_time_high': hist_alltime['High'].max(),
+            'pe': stock.info.get('forwardPE', None),
+            'peg': stock.info.get('pegRatio', None),
+            'market_cap': stock.info.get('marketCap', None),
+            'dividend_yield': stock.info.get('dividendYield', None),
+            'last_5_years_return': last_5_years_return
+        }
+    except Exception as e:
+        print(f"Error fetching data for symbol {ticker}: {e}")
+        return None
+
+def publish_to_google_sheet(df, spreadsheet_name, sheet_name):
+    # Authenticate with Pygsheets
+    gc = pygsheets.authorize()
+
+    # Open the spreadsheet and select the sheet
+    sh = gc.open(spreadsheet_name)
+    wks = sh.worksheet_by_title(sheet_name)
+
+    # Update the sheet with the DataFrame
+    df.fillna(0, inplace=True)
+    wks.set_dataframe(df, 'A1')
+
+def main():
+    with open('stocks.json') as f:
+        stocks = json.load(f)
+
+    data = []
+    for stock in stocks:
+        ticker = stock['ticker']
+        stock_data = get_stock_data(ticker)
+        if stock_data:
+            data.append(stock_data)
+
+    df = pd.DataFrame(data)
+    print(df)
+    
+    # Publish DataFrame to Google Sheet
+    spreadsheet_name = 'FinFun'
+    sheet_name = 'Data'
+    publish_to_google_sheet(df, spreadsheet_name, sheet_name)
+
+
+if __name__ == "__main__":
+    main()

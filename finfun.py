357d188c22f805c3a2637348537f5b1e80151a13
Ahmed Abdelfattah <ahmed.a.abdelfattah@mercedes-benz.com>
2024-03-31 09:57:38 +0200

    remove profiling

--- finfun.py
+++ finfun.py
@@ -1,5 +1,4 @@
 
-import cProfile
 import argparse
 import datetime
 from stocks_data_fetcher import StocksDataFetcher
@@ -10,7 +9,6 @@ from results_publisher import ResultsPublisher
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Analyzes stocks according to fundamental metrics.')
-    parser.add_argument('-p', '--profile', action='store_true', help='Enable profiling')
     parser.add_argument('-r', '--rank', type=int, required=True, help='Get top-ranked stocks up to specified rank')
 
     return parser.parse_args()
@@ -22,14 +20,10 @@ if __name__ == "__main__":
     fetcher = StocksDataFetcher()
     stocks_dict_by_sector = fetcher.stocks_dict_by_sector
     analyzer = StocksAnalyzer()
-    if args.profile:
-        import cProfile
-        cProfile.run('analyzer.analyze_stocks(stocks_dict_by_sector, args.rank)', sort='cumulative')
-    else:
-        analyzer.analyze_stocks(stocks_dict_by_sector, args.rank)
-        current_datetime = datetime.datetime.now()
-        top_ranked_stocks_string = f"{current_datetime.strftime('%Y%m%d_%H%M%S')}TopRankedStocks"
-        all_stocks_string = f"{current_datetime.strftime('%Y%m%d_%H%M%S')}AllStocks"
-        ResultsPublisher.publish_results(analyzer.all_stocks, output_format='excel', spreadsheet_name=all_stocks_string)
-        ResultsPublisher.publish_results(analyzer.top_ranked_stocks, output_format='excel', spreadsheet_name=top_ranked_stocks_string)
+    analyzer.analyze_stocks(stocks_dict_by_sector, args.rank)
+    current_datetime = datetime.datetime.now()
+    top_ranked_stocks_string = f"{current_datetime.strftime('%Y%m%d_%H%M%S')}TopRankedStocks"
+    all_stocks_string = f"{current_datetime.strftime('%Y%m%d_%H%M%S')}AllStocks"
+    ResultsPublisher.publish_results(analyzer.all_stocks, output_format='excel', spreadsheet_name=all_stocks_string)
+    ResultsPublisher.publish_results(analyzer.top_ranked_stocks, output_format='excel', spreadsheet_name=top_ranked_stocks_string)
 

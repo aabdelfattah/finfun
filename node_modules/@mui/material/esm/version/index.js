d4a41f16a2cb4a405f6de9308068ccb7cc94db5c
Ahmed Abdelfattah <a.abfattah@gmail.com>
2025-05-21 00:45:12 +0200

    chore: remove node_modules from git tracking

--- node_modules/@mui/material/esm/version/index.js
+++
@@ -1,6 +0,0 @@
-export const version = "7.1.0";
-export const major = Number("7");
-export const minor = Number("1");
-export const patch = Number("0");
-export const prerelease = undefined;
-export default version;
